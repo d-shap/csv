154beab5838546412f3e53bc8421bf7ba3b9179d
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-10-04 07:12:32 +0300

    Добавил javadoc

---
+++ src/main/java/ru/d_shap/csv/state/package-info.java
@@ -0,0 +1,10 @@
+// //////////////////////////////
+// This code is authored
+// Dmitry Shapovalov
+// //////////////////////////////
+/**
+ * <p>
+ * State machine for CSV parser.
+ * </p>
+ */
+package ru.d_shap.csv.state;
