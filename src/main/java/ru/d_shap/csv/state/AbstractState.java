66775396d4af88fa613b3f7e7a5d656bb63f34ef
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-10-05 07:18:23 +0300

    Добавил константы с символами, сделал метод publikc

--- src/main/java/ru/d_shap/csv/state/AbstractState.java
+++ src/main/java/ru/d_shap/csv/state/AbstractState.java
@@ -4,17 +4,39 @@
 // //////////////////////////////
 package ru.d_shap.csv.state;
 
+import ru.d_shap.csv.ColumnSeparators;
+import ru.d_shap.csv.RowSeparators;
+
 /**
  * Base class for all state classes.
  *
  * @author Dmitry Shapovalov
  */
-abstract class AbstractState {
+public abstract class AbstractState {
+
+    static final char END_OF_INPUT = 0;
+
+    static final char COMMA = ColumnSeparators.COMMA.getValue().charAt(0);
+
+    static final char SEMICOLON = ColumnSeparators.SEMICOLON.getValue().charAt(0);
+
+    static final char CR = RowSeparators.CR.getValue().charAt(0);
+
+    static final char LF = RowSeparators.LF.getValue().charAt(0);
+
+    static final char QUOT = '"';
 
     AbstractState() {
         super();
     }
 
-    abstract AbstractState processInput(char ch, Result result);
+    /**
+     * Process input char and define next parser state.
+     *
+     * @param ch     input symbol.
+     * @param result parse result object.
+     * @return next parser state.
+     */
+    public abstract AbstractState processInput(char ch, Result result);
 
 }
