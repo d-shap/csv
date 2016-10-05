9c90e9de27bec1aaf9047fa2b1a5943eaed06015
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-10-05 07:56:39 +0300

    Сделал константы public

--- src/main/java/ru/d_shap/csv/state/AbstractState.java
+++ src/main/java/ru/d_shap/csv/state/AbstractState.java
@@ -11,17 +11,17 @@ package ru.d_shap.csv.state;
  */
 public abstract class AbstractState {
 
-    static final char END_OF_INPUT = 0;
+    public static final char END_OF_INPUT = 0;
 
-    static final char COMMA = ',';
+    public static final char COMMA = ',';
 
-    static final char SEMICOLON = ';';
+    public static final char SEMICOLON = ';';
 
-    static final char CR = '\r';
+    public static final char CR = '\r';
 
-    static final char LF = '\n';
+    public static final char LF = '\n';
 
-    static final char QUOT = '"';
+    public static final char QUOT = '"';
 
     AbstractState() {
         super();
