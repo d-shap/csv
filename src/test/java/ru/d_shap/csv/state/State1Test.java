f1bea3a0dfd758f74681dd3cbed8a82fc186ded1
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-10-06 07:31:19 +0300

    Добавил тесты для состояний

---
+++ src/test/java/ru/d_shap/csv/state/State1Test.java
@@ -0,0 +1,133 @@
+// //////////////////////////////
+// This code is authored
+// Dmitry Shapovalov
+// //////////////////////////////
+package ru.d_shap.csv.state;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ru.d_shap.csv.CsvParser;
+
+/**
+ * Tests for {@link ru.d_shap.csv.state.State1}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class State1Test {
+
+    /**
+     * Test class constructor.
+     */
+    public State1Test() {
+        super();
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State1} class test.
+     */
+    @Test
+    public void processEndOfInputTest() {
+        String csv = ";";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(1, list.size());
+        Assert.assertEquals(2, list.get(0).size());
+        Assert.assertEquals("", list.get(0).get(0));
+        Assert.assertEquals("", list.get(0).get(1));
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State1} class test.
+     */
+    @Test
+    public void processCommaTest() {
+        String csv = ";,";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(1, list.size());
+        Assert.assertEquals(3, list.get(0).size());
+        Assert.assertEquals("", list.get(0).get(0));
+        Assert.assertEquals("", list.get(0).get(1));
+        Assert.assertEquals("", list.get(0).get(2));
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State1} class test.
+     */
+    @Test
+    public void processSemicolonTest() {
+        String csv = ";;";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(1, list.size());
+        Assert.assertEquals(3, list.get(0).size());
+        Assert.assertEquals("", list.get(0).get(0));
+        Assert.assertEquals("", list.get(0).get(1));
+        Assert.assertEquals("", list.get(0).get(2));
+
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State1} class test.
+     */
+    @Test
+    public void processCrTest() {
+        String csv = ";\ra";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(2, list.get(0).size());
+        Assert.assertEquals("", list.get(0).get(0));
+        Assert.assertEquals("", list.get(0).get(1));
+        Assert.assertEquals(1, list.get(1).size());
+        Assert.assertEquals("a", list.get(1).get(0));
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State1} class test.
+     */
+    @Test
+    public void processLfTest() {
+        String csv = ";\na";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(2, list.get(0).size());
+        Assert.assertEquals("", list.get(0).get(0));
+        Assert.assertEquals("", list.get(0).get(1));
+        Assert.assertEquals(1, list.get(1).size());
+        Assert.assertEquals("a", list.get(1).get(0));
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State1} class test.
+     */
+    @Test
+    public void processQuotTest() {
+        String csv = ";\"aa\"";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(1, list.size());
+        Assert.assertEquals(2, list.get(0).size());
+        Assert.assertEquals("", list.get(0).get(0));
+        Assert.assertEquals("aa", list.get(0).get(1));
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State1} class test.
+     */
+    @Test
+    public void processDefaultTest() {
+        String csv = ";aa";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(1, list.size());
+        Assert.assertEquals(2, list.get(0).size());
+        Assert.assertEquals("", list.get(0).get(0));
+        Assert.assertEquals("aa", list.get(0).get(1));
+    }
+
+}
