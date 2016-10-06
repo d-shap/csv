218612380082014641435c637e8822dfab4ce102
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-10-06 07:32:00 +0300

    Добавил тесты для состояний

---
+++ src/test/java/ru/d_shap/csv/state/State2Test.java
@@ -0,0 +1,124 @@
+// //////////////////////////////
+// This code is authored
+// Dmitry Shapovalov
+// //////////////////////////////
+package ru.d_shap.csv.state;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ru.d_shap.csv.CsvParser;
+
+/**
+ * Tests for {@link ru.d_shap.csv.state.State2}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class State2Test {
+
+    /**
+     * Test class constructor.
+     */
+    public State2Test() {
+        super();
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State2} class test.
+     */
+    @Test
+    public void processEndOfInputTest() {
+        String csv = "\n";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(1, list.size());
+        Assert.assertEquals(0, list.get(0).size());
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State2} class test.
+     */
+    @Test
+    public void processCommaTest() {
+        String csv = "\n,";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(0, list.get(0).size());
+        Assert.assertEquals(2, list.get(1).size());
+        Assert.assertEquals("", list.get(1).get(0));
+        Assert.assertEquals("", list.get(1).get(1));
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State2} class test.
+     */
+    @Test
+    public void processSemicolonTest() {
+        String csv = "\n;";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(0, list.get(0).size());
+        Assert.assertEquals(2, list.get(1).size());
+        Assert.assertEquals("", list.get(1).get(0));
+        Assert.assertEquals("", list.get(1).get(1));
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State2} class test.
+     */
+    @Test
+    public void processCrTest() {
+        String csv = "\n\r";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(0, list.get(0).size());
+        Assert.assertEquals(0, list.get(1).size());
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State2} class test.
+     */
+    @Test
+    public void processLfTest() {
+        String csv = "\n\n";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(0, list.get(0).size());
+        Assert.assertEquals(0, list.get(1).size());
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State2} class test.
+     */
+    @Test
+    public void processQuotTest() {
+        String csv = "\n\"aa\"";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(0, list.get(0).size());
+        Assert.assertEquals(1, list.get(1).size());
+        Assert.assertEquals("aa", list.get(1).get(0));
+    }
+
+    /**
+     * {@link ru.d_shap.csv.state.State2} class test.
+     */
+    @Test
+    public void processDefaultTest() {
+        String csv = "\naa";
+        List<List<String>> list = CsvParser.parseCsv(csv);
+        Assert.assertNotNull(list);
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(0, list.get(0).size());
+        Assert.assertEquals(1, list.get(1).size());
+        Assert.assertEquals("aa", list.get(1).get(0));
+    }
+
+}
