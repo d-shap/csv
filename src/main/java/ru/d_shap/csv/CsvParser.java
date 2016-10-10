b32b69088b9339d8c30924cde90389e8f885b56f
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-10-10 07:29:51 +0300

    Перегрузил конструкторы

--- src/main/java/ru/d_shap/csv/CsvParser.java
+++ src/main/java/ru/d_shap/csv/CsvParser.java
@@ -30,12 +30,23 @@ public final class CsvParser {
      * @return list of rows, each row is a list of columns.
      */
     public static List<List<String>> parseCsv(final String csv) {
+        return parseCsv(csv, false);
+    }
+
+    /**
+     * Parse CSV and define rows and columns.
+     *
+     * @param csv              CSV to parse.
+     * @param checkRectangular check if all rows should have the same column count.
+     * @return list of rows, each row is a list of columns.
+     */
+    public static List<List<String>> parseCsv(final String csv, final boolean checkRectangular) {
         if (csv == null) {
             return null;
         }
         Reader reader = new StringReader(csv);
         try {
-            return parseCsv(reader);
+            return parseCsv(reader, checkRectangular);
         } catch (IOException ex) {
             return null;
         }
@@ -49,11 +60,23 @@ public final class CsvParser {
      * @throws IOException IO Exception.
      */
     public static List<List<String>> parseCsv(final Reader reader) throws IOException {
+        return parseCsv(reader, false);
+    }
+
+    /**
+     * Parse CSV and define rows and columns.
+     *
+     * @param reader           CSV to parse.
+     * @param checkRectangular check if all rows should have the same column count.
+     * @return list of rows, each row is a list of columns.
+     * @throws IOException IO Exception.
+     */
+    public static List<List<String>> parseCsv(final Reader reader, final boolean checkRectangular) throws IOException {
         if (reader == null) {
             return null;
         }
 
-        ParserEventHandler parserEventHandler = new ParserEventHandler();
+        ParserEventHandler parserEventHandler = new ParserEventHandler(checkRectangular);
         AbstractState state = AbstractState.getInitState();
         int read;
         while (true) {
