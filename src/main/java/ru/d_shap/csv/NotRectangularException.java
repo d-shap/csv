bac0f36d926277ad6837af0441fcc8745e1841b8
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-10-21 07:32:36 +0300

    CSV не является прямоугольной

--- src/main/java/ru/d_shap/csv/NotRectangularException.java
+++ src/main/java/ru/d_shap/csv/NotRectangularException.java
@@ -24,35 +24,24 @@ package ru.d_shap.csv;
  *
  * @author Dmitry Shapovalov
  */
-public final class NotRectangularException extends RuntimeException {
+public class NotRectangularException extends CsvException {
 
     private static final long serialVersionUID = 1L;
 
     /**
-     * Creates new object.
+     * Create new object.
      */
     public NotRectangularException() {
-        super(getErrorMessage(null));
+        super("CSV is not rectangular.");
     }
 
     /**
-     * Creates new object.
+     * Create new object.
      *
-     * @param lastSymbols last symbols before wrong symbol.
+     * @param lastSymbols last symbols processed by CSV parser.
      */
     public NotRectangularException(final String lastSymbols) {
-        super(getErrorMessage(lastSymbols));
-    }
-
-    private static String getErrorMessage(final String lastSymbols) {
-        StringBuilder builder = new StringBuilder(80);
-        builder.append("CSV is not rectangular.");
-        if (lastSymbols != null) {
-            builder.append(" Last symbols: \"");
-            builder.append(lastSymbols);
-            builder.append("\".");
-        }
-        return builder.toString();
+        super("CSV is not rectangular.", lastSymbols);
     }
 
 }
