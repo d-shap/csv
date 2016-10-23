9637da338592707de0d5d08eed01e19be8f124fc
Dmitry@Dmitry-PC.Dlink <Dmitry@Dmitry-PC.Dlink>
2016-10-23 22:14:39 +0300

    Сделал запись CSV напрямую в поток

--- src/main/java/ru/d_shap/csv/CsvBuilder.java
+++ src/main/java/ru/d_shap/csv/CsvBuilder.java
@@ -22,14 +22,17 @@ package ru.d_shap.csv;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 import ru.d_shap.csv.state.AbstractState;
 
 /**
- * Class to create CSV from rows and columns. Object can be used to create multiple CSVs.
+ * Class to create CSV from rows and columns. If object is created with writer parameter, then column values
+ * and column and row separators are writen directly to the writer. If object is created without
+ * writer parameter, then StringWriter is used. In this case everithing is stored in memory. Method
+ * {@link #getCsv()} is a convenient method when StringWriter is used. This method returns generated CSV as String.
+ * For all other writer objects this method returns null.
  *
  * @author Dmitry Shapovalov
  */
@@ -46,23 +49,23 @@ public final class CsvBuilder {
         SPECIAL.add(RowSeparators.LF.getValue());
     }
 
+    private final Writer _writer;
+
     private final String _columnSeparator;
 
     private final String _rowSeparator;
 
     private final boolean _checkRectangular;
 
-    private List<List<String>> _rows;
+    private int _firstRowColumnCount;
 
-    private List<String> _currentRow;
-
-    private int _columnCount;
+    private int _currentColumnCount;
 
     /**
      * Create new object.
      */
     public CsvBuilder() {
-        this(ColumnSeparators.COMMA, RowSeparators.CRLF, false);
+        this(new StringWriter(), ColumnSeparators.COMMA, RowSeparators.CRLF, false);
     }
 
     /**
@@ -71,7 +74,7 @@ public final class CsvBuilder {
      * @param checkRectangular check if all rows should have the same column count.
      */
     public CsvBuilder(final boolean checkRectangular) {
-        this(ColumnSeparators.COMMA, RowSeparators.CRLF, checkRectangular);
+        this(new StringWriter(), ColumnSeparators.COMMA, RowSeparators.CRLF, checkRectangular);
     }
 
     /**
@@ -80,7 +83,7 @@ public final class CsvBuilder {
      * @param columnSeparator separator between columns.
      */
     public CsvBuilder(final ColumnSeparators columnSeparator) {
-        this(columnSeparator, RowSeparators.CRLF, false);
+        this(new StringWriter(), columnSeparator, RowSeparators.CRLF, false);
     }
 
     /**
@@ -90,7 +93,7 @@ public final class CsvBuilder {
      * @param checkRectangular check if all rows should have the same column count.
      */
     public CsvBuilder(final ColumnSeparators columnSeparator, final boolean checkRectangular) {
-        this(columnSeparator, RowSeparators.CRLF, checkRectangular);
+        this(new StringWriter(), columnSeparator, RowSeparators.CRLF, checkRectangular);
     }
 
     /**
@@ -99,7 +102,7 @@ public final class CsvBuilder {
      * @param rowSeparator separator between rows.
      */
     public CsvBuilder(final RowSeparators rowSeparator) {
-        this(ColumnSeparators.COMMA, rowSeparator, false);
+        this(new StringWriter(), ColumnSeparators.COMMA, rowSeparator, false);
     }
 
     /**
@@ -109,7 +112,7 @@ public final class CsvBuilder {
      * @param checkRectangular check if all rows should have the same column count.
      */
     public CsvBuilder(final RowSeparators rowSeparator, final boolean checkRectangular) {
-        this(ColumnSeparators.COMMA, rowSeparator, checkRectangular);
+        this(new StringWriter(), ColumnSeparators.COMMA, rowSeparator, checkRectangular);
     }
 
     /**
@@ -119,7 +122,7 @@ public final class CsvBuilder {
      * @param rowSeparator    separator between rows.
      */
     public CsvBuilder(final ColumnSeparators columnSeparator, final RowSeparators rowSeparator) {
-        this(columnSeparator, rowSeparator, false);
+        this(new StringWriter(), columnSeparator, rowSeparator, false);
     }
 
     /**
@@ -130,110 +133,183 @@ public final class CsvBuilder {
      * @param checkRectangular check if all rows should have the same column count.
      */
     public CsvBuilder(final ColumnSeparators columnSeparator, final RowSeparators rowSeparator, final boolean checkRectangular) {
+        this(new StringWriter(), columnSeparator, rowSeparator, checkRectangular);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param writer writer to write CSV.
+     */
+    public CsvBuilder(final Writer writer) {
+        this(writer, ColumnSeparators.COMMA, RowSeparators.CRLF, false);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param writer           writer to write CSV.
+     * @param checkRectangular check if all rows should have the same column count.
+     */
+    public CsvBuilder(final Writer writer, final boolean checkRectangular) {
+        this(writer, ColumnSeparators.COMMA, RowSeparators.CRLF, checkRectangular);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param writer          writer to write CSV.
+     * @param columnSeparator separator between columns.
+     */
+    public CsvBuilder(final Writer writer, final ColumnSeparators columnSeparator) {
+        this(writer, columnSeparator, RowSeparators.CRLF, false);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param writer           writer to write CSV.
+     * @param columnSeparator  separator between columns.
+     * @param checkRectangular check if all rows should have the same column count.
+     */
+    public CsvBuilder(final Writer writer, final ColumnSeparators columnSeparator, final boolean checkRectangular) {
+        this(writer, columnSeparator, RowSeparators.CRLF, checkRectangular);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param writer       writer to write CSV.
+     * @param rowSeparator separator between rows.
+     */
+    public CsvBuilder(final Writer writer, final RowSeparators rowSeparator) {
+        this(writer, ColumnSeparators.COMMA, rowSeparator, false);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param writer           writer to write CSV.
+     * @param rowSeparator     separator between rows.
+     * @param checkRectangular check if all rows should have the same column count.
+     */
+    public CsvBuilder(final Writer writer, final RowSeparators rowSeparator, final boolean checkRectangular) {
+        this(writer, ColumnSeparators.COMMA, rowSeparator, checkRectangular);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param writer          writer to write CSV.
+     * @param columnSeparator separator between columns.
+     * @param rowSeparator    separator between rows.
+     */
+    public CsvBuilder(final Writer writer, final ColumnSeparators columnSeparator, final RowSeparators rowSeparator) {
+        this(writer, columnSeparator, rowSeparator, false);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param writer           writer to write CSV.
+     * @param columnSeparator  separator between columns.
+     * @param rowSeparator     separator between rows.
+     * @param checkRectangular check if all rows should have the same column count.
+     */
+    public CsvBuilder(final Writer writer, final ColumnSeparators columnSeparator, final RowSeparators rowSeparator, final boolean checkRectangular) {
         super();
+        _writer = writer;
         _columnSeparator = columnSeparator.getValue();
         _rowSeparator = rowSeparator.getValue();
         _checkRectangular = checkRectangular;
-        _rows = null;
-        _currentRow = null;
-        _columnCount = -1;
-    }
-
-    private void setRows() {
-        if (_rows == null) {
-            _rows = new ArrayList<List<String>>();
-        }
-    }
-
-    private void setCurrentRow() {
-        if (_currentRow == null) {
-            _currentRow = new ArrayList<String>();
-        }
+        _firstRowColumnCount = -1;
+        _currentColumnCount = 0;
     }
 
     /**
-     * Add column to the current row and move cursor to the next column.
+     * Add column value to the current row.
      *
-     * @param column column.
+     * @param column column value.
      * @return current object for chaining.
      */
     public CsvBuilder addColumn(final int column) {
-        setCurrentRow();
-        _currentRow.add(String.valueOf(column));
+        doAddColumn(String.valueOf(column));
         return this;
     }
 
     /**
-     * Add column to the current row and move cursor to the next column.
+     * Add column value to the current row.
      *
-     * @param column column.
+     * @param column column value.
      * @return current object for chaining.
      */
     public CsvBuilder addColumn(final long column) {
-        setCurrentRow();
-        _currentRow.add(String.valueOf(column));
+        doAddColumn(String.valueOf(column));
         return this;
     }
 
     /**
-     * Add column to the current row and move cursor to the next column.
+     * Add column value to the current row.
      *
-     * @param column column.
+     * @param column column value.
      * @return current object for chaining.
      */
     public CsvBuilder addColumn(final float column) {
-        setCurrentRow();
-        _currentRow.add(String.valueOf(column));
+        doAddColumn(String.valueOf(column));
         return this;
     }
 
     /**
-     * Add column to the current row and move cursor to the next column.
+     * Add column value to the current row.
      *
-     * @param column column.
+     * @param column column value.
      * @return current object for chaining.
      */
     public CsvBuilder addColumn(final double column) {
-        setCurrentRow();
-        _currentRow.add(String.valueOf(column));
+        doAddColumn(String.valueOf(column));
         return this;
     }
 
     /**
-     * Add column to the current row and move cursor to the next column.
+     * Add column value to the current row.
      *
-     * @param column column.
+     * @param column column value.
      * @return current object for chaining.
      */
     public CsvBuilder addColumn(final boolean column) {
-        setCurrentRow();
-        _currentRow.add(String.valueOf(column));
+        doAddColumn(String.valueOf(column));
         return this;
     }
 
     /**
-     * Add column to the current row and move cursor to the next column.
+     * Add column value to the current row.
      *
-     * @param column column.
+     * @param column column value.
      * @return current object for chaining.
      */
-    public CsvBuilder addColumn(final String column) {
-        setCurrentRow();
-        _currentRow.add(getColumnForCsv(column));
+    public CsvBuilder addColumn(final Object column) {
+        doAddColumn(getColumnForCsv(column));
         return this;
     }
 
-    private String getColumnForCsv(final String column) {
+    private String getColumnForCsv(final Object column) {
         if (column == null) {
             return "";
         }
+        if (column instanceof String) {
+            return getColumnForCsv((String) column);
+        } else {
+            return getColumnForCsv(column.toString());
+        }
+    }
+
+    private String getColumnForCsv(final String column) {
         boolean hasSpecial = false;
         for (int i = 0; i < SPECIAL.size(); i++) {
             if (column.contains(SPECIAL.get(i))) {
                 hasSpecial = true;
             }
         }
-
         if (hasSpecial) {
             return "\"" + column.replaceAll("\"", "\"\"") + "\"";
         } else {
@@ -241,70 +317,50 @@ public final class CsvBuilder {
         }
     }
 
+    private void doAddColumn(final String column) {
+        try {
+            if (_currentColumnCount > 0) {
+                _writer.write(_columnSeparator);
+            }
+            _writer.write(column);
+            _currentColumnCount++;
+        } catch (IOException ex) {
+            throw new CsvIOException(ex);
+        }
+    }
+
     /**
-     * Complete current row and move cursor to the next row.
+     * Add row separator and start a new row.
      *
      * @return current object for chaining.
      */
     public CsvBuilder addRow() {
-        setRows();
-        setCurrentRow();
-        if (_columnCount < 0) {
-            _columnCount = _currentRow.size();
+        if (_firstRowColumnCount < 0) {
+            _firstRowColumnCount = _currentColumnCount;
         }
-        if (_checkRectangular && _columnCount != _currentRow.size()) {
+        if (_checkRectangular && _firstRowColumnCount != _currentColumnCount) {
             throw new NotRectangularException();
         }
-        _rows.add(_currentRow);
-        _currentRow = null;
+        try {
+            _writer.write(_rowSeparator);
+        } catch (IOException ex) {
+            throw new CsvIOException(ex);
+        }
+        _currentColumnCount = 0;
         return this;
     }
 
     /**
-     * Convert rows and columns to CSV and clear internal buffers. After this call builder can be used to create new CSV.
+     * Return created CSV, if StringWriter was used to create this object, implicitly or explicitly.
+     * Otherwise return null.
      *
-     * @return created CSV.
+     * @return created CSV or null, if NOT StringWriter was used to create this object.
      */
     public String getCsv() {
-        StringWriter writer = new StringWriter();
-        writeTo(writer);
-        return writer.getBuffer().toString();
-    }
-
-    /**
-     * Writes CSV to the writer. After this call builder can be used to create new CSV.
-     *
-     * @param writer writer to write CSV.
-     */
-    public void writeTo(final Writer writer) {
-        setRows();
-        convertToCsv(writer);
-        _rows = null;
-        _currentRow = null;
-        _columnCount = -1;
-    }
-
-    private void convertToCsv(final Writer writer) {
-        try {
-            int rowCount = _rows.size();
-            for (int rowIndex = 0; rowIndex < rowCount; rowIndex++) {
-                List<String> row = _rows.get(rowIndex);
-                int columnCount = row.size();
-                int columnCountM1 = columnCount - 1;
-                for (int columnIndex = 0; columnIndex <= columnCount; columnIndex++) {
-                    if (columnIndex == columnCount) {
-                        writer.write(_rowSeparator);
-                    } else {
-                        String column = row.get(columnIndex);
-                        writer.write(column);
-                        if (columnIndex != columnCountM1) {
-                            writer.write(_columnSeparator);
-                        }
-                    }
-                }
-            }
-        } catch (IOException ex) {
-            throw new CsvIOException(ex);
+        if (_writer instanceof StringWriter) {
+            return ((StringWriter) _writer).getBuffer().toString();
+        } else {
+            return null;
         }
     }
 
